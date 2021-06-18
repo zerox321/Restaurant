598b85bb2d351cb313ee95fb08f3611cee9daf90
eslam <eslamesw@gmail.com>
2021-06-18 17:44:02 +0200

    add Preview Images

--- app/src/main/java/com/example/mvvmEx/binding/RecyclerViewBindingAdapter.kt
+++ app/src/main/java/com/example/mvvmEx/binding/RecyclerViewBindingAdapter.kt
@@ -7,16 +7,7 @@ import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.example.mvvmEx.R
 import com.facebook.shimmer.ShimmerFrameLayout
 
-@BindingAdapter("bindSwipeRefreshLayoutColor")
-fun SwipeRefreshLayout.bindSwipeRefreshLayoutColor(attach: Boolean?) {
-    val c1 = ContextCompat.getColor(context, R.color.purple_200)
-    val c2 = ContextCompat.getColor(context, R.color.purple_500)
-    val c3 = ContextCompat.getColor(context, R.color.purple_700)
 
-    val c4 = ContextCompat.getColor(context, R.color.teal_200)
-    val c5 = ContextCompat.getColor(context, R.color.teal_700)
-    setColorSchemeColors(c1, c2, c3, c4, c5)
-}
 
 @BindingAdapter("bindShimmer")
 fun ShimmerFrameLayout.bindShimmer(isLoading: Boolean) {
